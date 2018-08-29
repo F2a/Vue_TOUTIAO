5f932c584a232d3371d379c6f9da7567453e3602
FangRui <arnold3000@163.com>
2018-08-29 17:43:22 +0800

    8.29 - header

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -1,17 +1,21 @@
 <template>
   <div>
-    <div class="header">
-      <mt-header
-        fixed
-        title="header"
-        class="home-header"
-        style="background-color: transparent"
-      >
-        <router-link to="/" slot="left">
-          <mt-button icon="back">返回</mt-button>
-        </router-link>
-        <mt-button icon="more" slot="right"></mt-button>
-      </mt-header>
+    <div ref="homeHeader" class="header">
+      <div :class="['home-header', {'backHeader': transform}]">
+        <a class="header-loc">
+          <i class="iconfont icon-loc-hollow" />
+          <span class="header-loc-txt">
+            <span class="header-loc-txt-marquee" data-address="">
+              弘林国际大厦
+            </span>
+          </span>
+          <i class="iconfont icon-arrow-right" />
+        </a>
+        <a class="search-icon-wrap">
+          <i style="padding: 0 8px" class="iconfont icon-search"></i>
+          <span class="search-text">搜索美团商家、商品名称</span>
+        </a>
+      </div>
       <div class="bnrs-wrap">
         <div id="bnrs" class="bnrs">
           <a class="baitem" href="javascript:;" data-app="1">
@@ -28,14 +32,87 @@
     data () {
       return {
         title: 'Content',
+        transform: false,
       }
     },
-    methods: {}
+    methods: {},
+    mounted() {
+      window.addEventListener('scroll', (e) => {
+        console.log(this.transform);
+        const scrollPosition = document.body.scrollTop + document.documentElement.scrollTop;
+        if(scrollPosition > 120&&!this.transform){
+          this.transform = true;
+        }else if(scrollPosition < 120&&this.transform){
+          this.transform = false;
+        }
+      })
+    },
   }
 </script>
-<style scoped>
+<style lang="less" rel="stylesheet/less" scoped>
   .header{
     margin-top: -60px;
+  }
+  .home-header {
+    position: fixed;
+    left: 0;
+    top: 0;
+    width: 100%;
+    padding: 10px 0;
+    z-index: 2;
+    text-align: center;
+    background: rgba(0,0,0,0);
+    transition: .3s;
+    &.backHeader{
+      background-color: rgba(255,181,28,.95);
+    }
+    .header-loc {
+      float: left;
+      height: 20px;
+      padding: 5px;
+      color: #ffffff;
+      font-size: 17px;
+      background: rgba(0,0,0,0.3);
+      -webkit-border-radius: 13px;
+      -moz-border-radius: 13px;
+      border-radius: 13px;
+      margin-left: 10px;
+      position: relative;
+      max-width: 40%;
+      margin-right: 12px;
+      .header-loc-txt {
+        display: inline-block;
+        box-sizing: border-box;
+        max-width: 100%;
+        overflow: hidden;
+        vertical-align: middle;
+        padding: 0 10px;
+        .header-loc-txt-marquee {
+          white-space: nowrap;
+          display: inline-block;
+          max-width: 100%;
+          overflow: hidden;
+          text-overflow: ellipsis;
+          font-size: 14px;
+        }
+      }
+    }
+    .search-icon-wrap {
+      float: right;
+      height: 28px;
+      padding-top: 4px;
+      box-sizing: border-box;
+      border-radius: 13px;
+      font-size: 0;
+      margin-right: 10px;
+      width: 45%;
+      background: #fff;
+      text-align: left;
+      .search-text {
+        color: #999;
+        font-size: 0.25rem;
+      }
+    }
   }
   .bnrs-wrap {
     width: 100%;
