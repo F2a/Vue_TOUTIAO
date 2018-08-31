26a9e2f9406f906c7d7bb7f5419637bb30c2341c
FangRui <arnold3000@163.com>
2018-08-31 16:49:59 +0800

    8.31 - seller

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from '@/models/Home';
+import Seller from '@/models/Seller';
 
 Vue.use(Router);
 
@@ -10,6 +11,15 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home,
+      redirect: '/home',
+      children: [
+        { path: 'home', component: Home, name: '首页' }
+      ]
+    },
+    {
+      path: '/seller',
+      name: 'Seller',
+      component: Seller,
     },
   ],
 });
