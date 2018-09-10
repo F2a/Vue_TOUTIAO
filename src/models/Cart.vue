00bcf3442bb320aff64200d9d7def02d46c4b42f
FangRui <arnold3000@163.com>
2018-09-10 10:00:15 +0800

    9.10 - cart

---
+++ src/models/Cart.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+
+  </div>
+</template>
+<script>
+  export default {
+    name: 'Cart',
+    data () {
+      return {
+        title: 'Cart',
+      }
+    },
+    methods: {}
+  }
+</script>
+<style lang="less" rel="stylesheet/less" scoped>
+
+</style>
