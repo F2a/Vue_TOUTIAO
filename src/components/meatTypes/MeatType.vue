fe7b2c1c9b119929bb89e75c0eda308f6489ded5
FangRui <arnold3000@163.com>
2018-10-10 18:04:22 +0800

    10.10 - search

--- src/components/meatTypes/MeatType.vue
+++ src/components/meatTypes/MeatType.vue
@@ -1,36 +1,52 @@
 <template>
   <div>
     <div class="clearfix typesBox">
-      <a class="types">
-        <div class="circle"></div>
+      <a class="types" @click="toSearch('美食')">
+        <div class="circle" style="background-color: #F7B55D">
+          <i class="iconfont icon-meishi"></i>
+        </div>
         <span>美食</span>
       </a>
-      <a class="types">
-        <div class="circle"></div>
+      <a class="types" @click="toSearch('美团城市')">
+        <div class="circle" style="background-color: #9874DB">
+          <i class="iconfont icon-chaoshi"></i>
+        </div>
         <span>美团城市</span>
       </a>
-      <a class="types">
-        <div class="circle"></div>
+      <a class="types" @click="toSearch('生鲜蔬果')">
+        <div class="circle" style="background-color: #17C295">
+          <i class="iconfont icon-ziyuan"></i>
+        </div>
         <span>生鲜蔬果</span>
       </a>
-      <a class="types">
-        <div class="circle"></div>
+      <a class="types" @click="toSearch('美团专递')">
+        <div class="circle" style="background-color: #F7B55D">
+          <i class="iconfont icon-meituanwaimai"></i>
+        </div>
         <span>美团专递</span>
       </a>
-      <a class="types">
-        <div class="circle"></div>
+      <a class="types" @click="toSearch('晚餐优选')">
+        <div class="circle" style="background-color: #17C295">
+          <i class="iconfont icon-wancan"></i>
+        </div>
         <span>晚餐优选</span>
       </a>
-      <a class="types">
-        <div class="circle"></div>
+      <a class="types" @click="toSearch('甜点饮品')">
+        <div class="circle" style="background-color: #4EA8EC">
+          <i class="iconfont icon-tianpin"></i>
+        </div>
         <span>甜点饮品</span>
       </a>
-      <a class="types">
-        <div class="circle"></div>
+      <a class="types" @click="toSearch('小吃馆')">
+        <div class="circle" style="background-color: #9874DB">
+          <i class="iconfont icon-xiaochi14"></i>
+        </div>
         <span>小吃馆</span>
       </a>
-      <a class="types">
-        <div class="circle"></div>
+      <a class="types" @click="toSearch('家常菜')">
+        <div class="circle" style="background-color: #F2725D">
+          <i class="iconfont icon-chaocaishouru"></i>
+        </div>
         <span>家常菜</span>
       </a>
     </div>
@@ -42,9 +58,15 @@
     data () {
       return {
         title: 'MeatType',
+        key: ''
       }
     },
-    methods: {}
+    methods: {
+      toSearch(key) {
+        this.$router.push({path: '/search', query: { key }});
+      }
+    },
+
   }
 </script>
 <style scoped>
@@ -65,5 +87,10 @@
     margin: 0.15rem auto;
     border-radius: 50%;
     background-color: #ccc;
+  }
+  .iconfont{
+    font-size: .9rem;
+    line-height: 1.35rem;
+    color: #FFFFFF;
   }
 </style>
--- src/models/Search.vue
+++ src/models/Search.vue
@@ -1,19 +1,96 @@
 <template>
   <div>
-
+    <div class="headerBackground" />
+    <section class="header">
+      <router-link :to="{ path: 'home'}" replace style="padding: 0 5px">
+        <a class="back" />
+      </router-link>
+      <form class="fromBox">
+        <i class="iconfont icon-search"></i>
+        <input v-model="key" type="search" placeholder="输入商家、商品名称">
+        <button type="submit">搜索</button>
+      </form>
+    </section>
+    <v-sellerList :marginTop="50" />
   </div>
 </template>
 <script>
+  import sellerList from '../components/sellerList/SellerList.vue';
+
   export default {
     name: 'Search',
     data () {
       return {
         title: 'Search',
+        key: '',
       }
     },
-    methods: {}
+    methods: {},
+    mounted() {
+      console.log(this.$route.query.key);
+      this.key = this.$route.query.key;
+    },
+    components: {
+      "v-sellerList": sellerList,
+    },
   }
 </script>
-<style>
-
+<style lang="less" rel="stylesheet/less" scoped>
+  .headerBackground {
+    width: 100%;
+    height: 50px;
+  }
+  .header {
+    display: flex;
+    position: fixed;
+    top: 0;
+    width: 100%;
+    height: 50px;
+    z-index: 101;
+    background: #fff;
+    .fromBox {
+      display: flex;
+      position: relative;
+      flex-grow: 1;
+      padding: .293333rem .293333rem .293333rem 0;
+      margin-left: .293333rem;
+      background-color: #fff;
+      z-index: 101;
+      i {
+        position: absolute;
+        top: .45rem;
+        left: .22rem;
+      }
+      input {
+        -webkit-box-flex: 1;
+        flex-grow: 1;
+        border-radius: .026667rem;
+        -webkit-appearance: none;
+        background-color: #f8f8f8;
+        padding: .173333rem .4rem .173333rem .88rem;
+        font-size: .346667rem;
+        color: #666;
+        border: none;
+        outline: none;
+      }
+      button {
+        outline: none;
+        color: #333;
+        font-size: .426667rem;
+        background: #fff;
+        font-weight: 700;
+        margin-left: .293333rem;
+        border: none;
+      }
+    }
+  }
+  .back {
+    margin-left: .35rem;
+    width: .32rem;
+    height: .32rem;
+    display: inline-block;
+    border-bottom: .05rem solid #999;
+    border-left: .05rem solid #999;
+    transform: rotate(45deg);
+  }
 </style>
