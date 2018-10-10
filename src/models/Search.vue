a7d3386b1ac8adf8da817d879ca01e0458d8a5c8
FangRui <arnold3000@163.com>
2018-10-10 16:34:04 +0800

    10.8 - cart

---
+++ src/models/Search.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+
+  </div>
+</template>
+<script>
+  export default {
+    name: 'Search',
+    data () {
+      return {
+        title: 'Search',
+      }
+    },
+    methods: {}
+  }
+</script>
+<style>
+
+</style>
