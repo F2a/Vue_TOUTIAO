6762010315c529703bbea563fc276afa2115141e
FangRui <arnold3000@163.com>
2018-10-16 17:28:57 +0800

    10.16 - location

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import seller from './modules/seller'
+import location from './modules/location'
 import getters from './getters'
 
 Vue.use(Vuex)
@@ -8,6 +9,7 @@ Vue.use(Vuex)
 const store = new Vuex.Store({
   modules: {
     seller,
+    location,
   },
   getters
 })
