eb9714aab7c025e05b0435de2392cc1686a7e9bf
FangRui <arnold3000@163.com>
2018-10-16 17:23:18 +0800

    10.16 - location

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,9 +1,10 @@
 const getters = {
+  /*loation*/
+  position: state => state.location.position,
   /*seller*/
   sellerList: state => state.seller.sellerList,
   goods: state => state.seller.goods,
   cart: state => state.seller.cart,
   sellerSyn: state => state.seller.sellerSyn,
-
 }
 export default getters
